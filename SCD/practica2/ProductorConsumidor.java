ed43985ce7abbc123306049488a17a1f845f9dac
Montse Rodríguez Zamorano <montserzamorano@correo.ugr.es>
2015-12-20 18:12:57 +0100

    Update ProductorConsumidor.java

--- SCD/practica2/ProductorConsumidor.java
+++ SCD/practica2/ProductorConsumidor.java
@@ -146,11 +146,11 @@ class MainProductorConsumidor
     }
 
     // leer parametros, crear vectores y buffer intermedio
-    Consumidor[] cons      = new Consumidor[Integer.parseInt(args[0])] ;
-	  Productor[]  prod      = new Productor[Integer.parseInt(args[1])] ;
-	  Buffer       buffer    = new Buffer(Integer.parseInt(args[2]));
-	  int          iter_cons = Integer.parseInt(args[3]);
-	  int          iter_prod = Integer.parseInt(args[4]);
+    Consumidor[] cons = new Consumidor[Integer.parseInt(args[0])] ;
+    Productor[]  prod = new Productor[Integer.parseInt(args[1])] ;
+    Buffer buffer = new Buffer(Integer.parseInt(args[2]));
+    int iter_cons = Integer.parseInt(args[3]);
+    int iter_prod = Integer.parseInt(args[4]);
 
     if ( cons.length*iter_cons != prod.length*iter_prod )
 	  {
@@ -166,8 +166,8 @@ class MainProductorConsumidor
 
 	  // poner en marcha las hebras
 	  for(int i = 0; i < prod.length; i++)
-      prod[i].thr.start();
+	  	prod[i].thr.start();
 	  for(int i = 0; i < cons.length; i++)
-      cons[i].thr.start();
+      		cons[i].thr.start();
   }
 }
