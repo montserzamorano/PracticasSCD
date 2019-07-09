740170f3f7e35b7828a3d9f093cc48424879b55c
Montse Rodríguez Zamorano <montserzamorano@correo.ugr.es>
2019-07-09 21:35:27 +0200

    commit
    actualización

---
+++ practica2/codigo_adicional/barberia_limite.java
@@ -0,0 +1,175 @@
+/******************************************************************************
+EJERCICIO 3. EL PROBLEMA DE LA BARBERIA
+Montserrat Rodriguez Zamorano
+Doble Grado en Ingenieria Informatica y Matematicas. Curso 2015/2016
+
+Colores para la consola
+
+public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_BLACK = "\u001B[30m";
+public static final String ANSI_RED = "\u001B[31m";
+public static final String ANSI_GREEN = "\u001B[32m";
+public static final String ANSI_YELLOW = "\u001B[33m";
+public static final String ANSI_BLUE = "\u001B[34m";
+public static final String ANSI_PURPLE = "\u001B[35m";
+public static final String ANSI_CYAN = "\u001B[36m";
+public static final String ANSI_WHITE = "\u001B[37m";
+
+Para terminar, poner ANSI_RESET. Ejemplo
+
+System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
+
+Nota: A veces la frase ¡que pase el siguiente cliente! no aparece.
+Esto es porque solo aparece esta frase si se entra en el procedimiento
+siguienteCliente(). Hay veces en las que el cliente va a sentarse a esperar
+justo en el momento en el que la silla se libera, así que no se sienta y
+pasa directamente, con lo que la silla se ocupa y el barbero no pasa al
+procedimiento siguienteCliente().
+
+******************************************************************************/
+
+import monitor.*;
+
+class Barberia extends AbstractMonitor{
+  private Condition salaEspera;
+  private Condition barbero;
+  private Condition silla;
+  private int limite_clientes = 2;
+  private int clientes_esperando = 0;
+  public Barberia(){
+    salaEspera = makeCondition();
+    barbero = makeCondition();
+    silla = makeCondition();
+  }
+  //invocado por los clientes para cortarse el pelo
+  public void cortarPelo(int i){
+    enter();
+    if(clientes_esperando < limite_clientes){
+      if(!silla.isEmpty()){ //si el barbero esta ocupado
+        System.out.println("\u001B[32m" + "El cliente " + i
+        + " se sienta a esperar en la sala de espera."+ "\u001B[0m");
+        clientes_esperando=clientes_esperando+1;
+        salaEspera.await(); //esperar en la sala de espera
+      }
+      //cliente liberado
+      clientes_esperando--;
+      if(!barbero.isEmpty()){ //si el barbero esta durmiendo
+        barbero.signal(); //liberar al barbero
+      }
+      System.out.println("\u001B[32m" + "Cliente "
+      + i + " entra a cortarse el pelo." + "\u001B[0m");
+      silla.await(); //ocupar la silla
+    }
+    else{
+      System.out.println("\u001B[32m" + "Cliente "
+      + i + " sale de la barbería porque está llena." + "\u001B[0m");
+    }
+
+    leave();
+  }
+  //invocado por el barbero para esperar(si procede) a un nuevo cliente
+  //y sentarlo para el cortar
+  public void siguienteCliente(){
+    enter();
+    //si no hay nadie en la sala de espera ni en la silla, dormir
+    if(salaEspera.isEmpty() && silla.isEmpty()){
+      System.out.println("\u001B[33m" + "Parece que no hay nadie..." + "\u001B[0m");
+      System.out.println("\u001B[33m" + "zZzzZzZZzzzZzZZ" + "\u001B[0m");
+      barbero.await(); //zZzzZ
+    }
+    else if(!salaEspera.isEmpty()){
+      System.out.println("\u001B[36m"
+      + "¡Que pase el siguiente cliente!" +"\u001B[0m");
+      salaEspera.signal(); //dejamos pasar al siguiente
+    }
+
+    leave();
+  }
+  //invocado por el barbero para indicar que ha terminado de cortar el pelo
+  public void finCliente(){
+    enter();
+
+    System.out.println("\u001B[36m"
+    + "Ya he terminado de afeitar al cliente." + "\u001B[0m");
+    silla.signal(); //liberamos la silla
+
+    leave();
+  }
+}
+
+class Cliente implements Runnable{
+  public Thread thr;
+  private int num_cliente;
+  private Barberia barberia;
+  public Cliente(Barberia barberia,int num){
+    num_cliente = num + 1;
+    this.barberia = barberia;
+    thr = new Thread(this,"cliente " + num_cliente);
+  }
+  public void run(){
+    while(true){
+      System.out.println("\u001B[35m" + "El " + thr.getName()
+      + " ha entrado en la barbería." + "\u001B[0m");
+      try{
+        Thread.sleep(2500); //el cliente está fuera de la barbería un tiempo.
+      }
+      catch(InterruptedException e){
+        System.err.println("Cliente interrumpido");
+      }
+      barberia.cortarPelo(num_cliente); //el cliente espera(si procede) y se corta el pelo
+      try{
+        Thread.sleep(25000); //el cliente está fuera de la barbería un tiempo.
+      }
+      catch(InterruptedException e){
+        System.err.println("Cliente interrumpido");
+      }
+    }
+  }
+}
+
+class Barbero implements Runnable{
+  public Thread thr;
+  private Barberia barberia = new Barberia();
+  public Barbero(Barberia barberia){
+    this.barberia = barberia;
+    thr = new Thread(this, "barbero");
+  }
+  public void run(){
+    while(true){
+      try{
+        Thread.sleep(2500); //el barbero esta cortando el pelo
+        barberia.finCliente();
+        barberia.siguienteCliente();
+      }
+      catch(InterruptedException e){
+        System.err.println("Barbero interrumpido");
+      }
+    }
+  }
+}
+
+class MainBarberiaFin
+{
+  public static void main( String[] args )
+  {
+    final int num_clientes = 5;
+    Barberia barberia = new Barberia();
+    Barbero b = new Barbero(barberia);
+    Cliente[] c = new Cliente[num_clientes];
+    for(int i= 0; i < num_clientes; i++){
+      c[i] = new Cliente(barberia,i);
+    }
+
+    // crear hebras
+
+    for(int i = 0; i < c.length; i++){
+      c[i] = new Cliente(barberia,i);
+    }
+
+    // poner en marcha las hebras
+    for(int i = 0; i < c.length; i++)
+      c[i].thr.start();
+
+    b.thr.start();
+  }
+}
---
+++ practica2/codigo_adicional/fumadores_fin.java
@@ -0,0 +1,206 @@
+/******************************************************************************
+EJERCICIO 2. EL PROBLEMA DE LOS FUMADORES
+Montserrat Rodriguez Zamorano
+Doble Grado en Ingenieria Informatica y Matematicas. Curso 2015/2016
+
+Colores para la consola
+
+public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_BLACK = "\u001B[30m";
+public static final String ANSI_RED = "\u001B[31m";
+public static final String ANSI_GREEN = "\u001B[32m";
+public static final String ANSI_YELLOW = "\u001B[33m";
+public static final String ANSI_BLUE = "\u001B[34m";
+public static final String ANSI_PURPLE = "\u001B[35m";
+public static final String ANSI_CYAN = "\u001B[36m";
+public static final String ANSI_WHITE = "\u001B[37m";
+
+Para terminar, poner ANSI_RESET. Ejemplo
+
+System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
+
+******************************************************************************/
+
+import monitor.*;
+import java.util.Random;
+
+//monitor estanco
+///////////////////// CLASE ESTANCO /////////////////////////////////////////
+
+class Estanco extends AbstractMonitor{
+  String [] arrayIngredientes = {"cerillas", "tabaco", "papel"};
+  int num_fumadores = 3; //visibilidad de paquete
+  private int limite = 10;
+  boolean abierto;
+  private Condition[] condFumadores = new Condition[3];
+  private Condition condEstanqueros;
+  private int sobre_el_mostrador;
+  private int contador;
+  public Estanco(){
+    condEstanqueros=makeCondition(); //inicializar estanquero
+    contador = 0;
+    abierto = true;
+    for(int i=0; i<num_fumadores; i++){ //inicializar fumadores
+      condFumadores[i] = makeCondition();
+    }
+    sobre_el_mostrador=-1; //inicializar variable mostrador
+  }
+  //getter contador
+  public int getContador(){
+    return contador;
+  }
+  //getter limite
+  public int getLimite(){
+    return limite;
+  }
+  //invocado por cada fumador, indicando su ingrediente o numero
+  public void obtenerIngrediente(int miIngrediente){
+    enter();
+
+    if(sobre_el_mostrador != miIngrediente){  //si no hay ingrediente o es
+                                              //distinto al suyo
+      condFumadores[miIngrediente].await();   //esperar
+    }
+    System.out.println("\u001B[36m" +
+    "Fumador " + (miIngrediente+1) + " recoge " +
+    arrayIngredientes[miIngrediente] + "." + "\u001B[0m");
+    sobre_el_mostrador=-1; //el mostrador queda vacío
+    condEstanqueros.signal(); //liberar al estanquero
+
+    leave();
+  }
+  //invocado por el estanquero, indicando el ingrediente que pone
+
+  public void ponerIngrediente(int ingrediente){
+    enter();
+    if(contador < limite){
+      sobre_el_mostrador = ingrediente; //ponerlo sobre el mostrador
+      contador++;
+      System.out.println("\u001B[33m" +
+      "El estanquero coloca " + arrayIngredientes[ingrediente]
+      + " sobre el mostrador" + "\u001B[0m");
+      condFumadores[ingrediente].signal(); //liberar al fumador correspondiente
+    }
+    else{
+      System.out.println("\u001B[33m" +
+      "El estanquero cierra el estanco." + "\u001B[0m");
+      abierto = false;
+      for(int i=0; i<num_fumadores; i++){
+          condFumadores[i].signal();
+      }
+      System.out.println("\u001B[33m" +
+      "Hasta mañana." + "\u001B[0m");
+    }
+
+    leave();
+  }
+  //invocado por el estanquero
+
+  public void esperarRecogidaIngrediente(){
+    enter();
+
+    if(sobre_el_mostrador != -1){ //si el mostrador no está vacío, dormir
+      condEstanqueros.await();
+    }
+
+    leave();
+  }
+}
+////////////////////////////FIN CLASE ESTANCO/////////////////////////////////
+
+////////////////////////////CLASE AUX/////////////////////////////////////////
+
+/*class aux{
+  static Random genAlea = new Random();
+  static void dormir_max(int milisecsMax){
+    try{
+      Thread.sleep(genAlea.nextInt(milisecsMax));
+    }catch(InterruptedException e){
+      System.err.println("Sleep interrumpido.");
+    }
+  }
+}*/
+
+///////////////////////////FIN CLASE AUX//////////////////////////////////////
+
+////////////////////////////CLASE FUMADOR/////////////////////////////////////
+
+//hebra fumador
+class Fumador implements Runnable {
+  int miIngrediente;
+  private Estanco estanco = new Estanco();
+  public Thread thr; //objeto hebra encapsulado
+  //constructor
+  public Fumador(Estanco estanco, int p_miIngrediente){
+    miIngrediente = p_miIngrediente;
+    this.estanco = estanco;
+    thr = new Thread(this,"Fumador " + (p_miIngrediente+1));
+  } //Faltan parámetros
+  public void run(){
+    System.out.println("\u001B[36m" +
+    thr.getName() + " llega al estanco." + "\u001B[0m");
+    while(estanco.abierto)
+    {
+      estanco.obtenerIngrediente(miIngrediente); //coger el ingrediente
+      try{
+        //aux.dormir_max(2000);
+        Thread.sleep(2000);
+        System.out.println("\u001B[36m" +
+        thr.getName() + " termina de fumar." + "\u001B[0m");
+      }
+      catch(InterruptedException e){
+        System.err.println("Fumador interrumpido");
+      }
+    }
+    System.out.println("\u001B[36m" +
+    thr.getName() + " se va a su casa." + "\u001B[0m");
+  }
+}
+/////////////////////////////FIN CLASE FUMADOR////////////////////////////////
+
+////////////////////////////CLASE ESTANQUERO//////////////////////////////////
+//hebra estanquero
+class Estanquero implements Runnable {
+  public Thread thr; //objeto hebra encapsulado
+  private Estanco estanco = new Estanco();
+  int ingrediente;
+  //constructor
+  public Estanquero(Estanco estanco){
+    this.estanco=estanco;
+    thr = new Thread(this,"Estanquero");
+  }
+  public void run(){
+    System.out.println("\u001B[33m" +
+    thr.getName() + " abre el estanco." + "\u001B[0m");
+    while (estanco.abierto){
+        ingrediente = (int) (Math.random() * 3.0); //0,1,2
+        estanco.ponerIngrediente(ingrediente); //poner el ingrediente en el mostrador
+        estanco.esperarRecogidaIngrediente(); //esperar a que el fumador lo recoja
+    }
+  }
+}
+
+///////////////////////// FIN CLASE ESTANQUERO ////////////////////////////////
+
+/////////////////////////////// MAIN ///////////////////////////////////////
+
+class MainFumadoresFinito
+{
+  public static void main( String[] args )
+  {
+    int num_fumadores = 3;
+    Estanco estanco = new Estanco();
+    Fumador[] f = new Fumador[num_fumadores];
+	  // crear hebras
+    Estanquero e = new Estanquero(estanco) ;
+
+	  for(int i = 0; i < f.length; i++){
+	    f[i] = new Fumador(estanco,i);
+    }
+
+	  // poner en marcha las hebras
+    e.thr.start();
+	  for(int i = 0; i < f.length; i++)
+      f[i].thr.start();
+    }
+}
